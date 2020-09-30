be045e50d02cda902f201ab827bfb726264b15fc
bingblackbean <bean.ren2016@outlook.com>
2020-09-30 23:19:53 +0200

    Initial commit

---
+++ bearing_abnormal_detection_3rd_test.py
@@ -0,0 +1,167 @@
+# Common imports
+import os
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import seaborn as sns
+sns.set(color_codes=True)
+import matplotlib.pyplot as plt
+
+from numpy.random import seed
+#from tensorflow import set_random_seed
+import tensorflow as tf
+from keras.layers import Input, Dropout
+from keras.layers.core import Dense
+from keras.models import Model, Sequential, load_model
+from keras import regularizers
+from keras.models import model_from_json
+
+"""
+resampling the data by 10 minutes
+"""
+# folder = r'C:\Users\renb\Downloads\nasa\3rd_test'
+# data_dir = folder+'\\4th_test\\txt'
+# merged_data = pd.DataFrame()
+#
+# for filename in os.listdir(data_dir):
+#     print(filename)
+#     dataset=pd.read_csv(os.path.join(data_dir, filename), sep='\t')
+#     dataset_mean_abs = np.array(dataset.abs().mean())
+#     dataset_mean_abs = pd.DataFrame(dataset_mean_abs.reshape(1,4))
+#     dataset_mean_abs.index = [filename]
+#     merged_data = merged_data.append(dataset_mean_abs)
+# merged_data.reset_index(inplace=True)  # reset index to get datetime as columns
+# merged_data.columns = ['Datetime','Bearing 1','Bearing 2','Bearing 3','Bearing 4'] # rename columns
+# merged_data.sort_values(by='Datetime',inplace=True)
+# merged_data.to_csv('3rd_test_resmaple_10minutes.csv')
+
+"""
+read from new dataset if you don't want to download the original dataset
+"""
+
+merged_data = pd.read_csv('3rd_test_resmaple_10minutes.csv',index_col='Datetime',usecols=['Datetime','Bearing 1','Bearing 2','Bearing 3','Bearing 4'])
+merged_data.index = pd.to_datetime(merged_data.index, format='%Y.%m.%d.%H.%M.%S')
+print(merged_data.head())
+
+merged_data.plot()
+
+merged_data.index.max()
+merged_data.index.min()
+"""
+split train and test dataset
+"""
+
+
+dataset_train = merged_data['2004-03-04 09:27:46':'2004-04-01 00:01:57']
+dataset_test = merged_data['2004-04-01 00:01:57':]
+dataset_train.plot(figsize = (12,6))
+
+"""
+Normalize data
+"""
+scaler = preprocessing.MinMaxScaler()
+
+X_train = pd.DataFrame(scaler.fit_transform(dataset_train),
+                              columns=dataset_train.columns,
+                              index=dataset_train.index)
+# Random shuffle training data
+X_train.sample(frac=1)
+
+X_test = pd.DataFrame(scaler.transform(dataset_test),
+                             columns=dataset_test.columns,
+                             index=dataset_test.index)
+X_test.plot()
+"""
+PCA type model for anomaly detection
+"""
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2, svd_solver= 'full')
+X_train_PCA = pca.fit_transform(X_train)
+X_train_PCA = pd.DataFrame(X_train_PCA)
+X_train_PCA.index = X_train.index
+
+X_test_PCA = pca.transform(X_test)
+X_test_PCA = pd.DataFrame(X_test_PCA)
+X_test_PCA.index = X_test.index
+
+X_train_PCA.plot()
+X_test_PCA.plot()
+
+def is_pos_def(A):
+    if np.allclose(A, A.T): # check if A is Symmetric Matrices
+        try:
+            np.linalg.cholesky(A)
+            return True
+        except np.linalg.LinAlgError:
+            return False
+    else:
+        return False
+
+def cov_matrix(data):
+    covariance_matrix = np.cov(data, rowvar=False)
+    if is_pos_def(covariance_matrix):
+        inv_covariance_matrix = np.linalg.inv(covariance_matrix)
+        if is_pos_def(inv_covariance_matrix):
+            return covariance_matrix, inv_covariance_matrix
+        else:
+            print("Error: Inverse of Covariance Matrix is not positive definite!")
+    else:
+        print("Error: Covariance Matrix is not positive definite!")
+
+def MahalanobisDist(inv_cov_matrix, mean_distr, data, verbose=False):
+    inv_covariance_matrix = inv_cov_matrix
+    vars_mean = mean_distr
+    diff = data - vars_mean
+    md = []
+    for i in range(len(diff)):
+        md.append(np.sqrt(diff[i].dot(inv_covariance_matrix).dot(diff[i])))
+    return md
+
+
+def MD_threshold(dist, extreme=False, verbose=False):
+    k = 4. if extreme else 2.3
+    threshold = np.mean(dist) * k
+    return threshold
+
+data_train = np.array(X_train_PCA.values)
+data_test = np.array(X_test_PCA.values)
+cov_matrix, inv_cov_matrix  = cov_matrix(data_train)
+mean_distr = data_train.mean(axis=0)
+
+dist_test = MahalanobisDist(inv_cov_matrix, mean_distr, data_test, verbose=False)
+dist_train = MahalanobisDist(inv_cov_matrix, mean_distr, data_train, verbose=False)
+threshold = MD_threshold(dist_train, extreme = True)
+plt.figure()
+sns.distplot(np.square(dist_train),
+             bins = 10,
+             kde= False)
+plt.xlim([0.0,15])
+
+plt.figure()
+sns.distplot(dist_train,
+             bins = 10,
+             kde= True,
+            color = 'green')
+plt.xlim([0.0,5])
+plt.xlabel('Mahalanobis dist')
+
+anomaly_train = pd.DataFrame()
+anomaly_train['Mob dist']= dist_train
+anomaly_train['Thresh'] = threshold
+# If Mob dist above threshold: Flag as anomaly
+anomaly_train['Anomaly'] = anomaly_train['Mob dist'] > anomaly_train['Thresh']
+anomaly_train.index = X_train_PCA.index
+anomaly = pd.DataFrame()
+anomaly['Mob dist']= dist_test
+anomaly['Thresh'] = threshold
+# If Mob dist above threshold: Flag as anomaly
+anomaly['Anomaly'] = anomaly['Mob dist'] > anomaly['Thresh']
+anomaly.index = X_test_PCA.index
+print(anomaly.head())
+anomaly_alldata = pd.concat([anomaly_train, anomaly])
+anomaly_alldata.to_csv('Anomaly_distance.csv')
+
+anomaly_alldata.plot(logy=True, figsize = (10,6), ylim = [1e-1,1e3], color = ['green','red'])
+
+
+print(anomaly_alldata[anomaly_alldata['Anomaly']==1].index)
